e6874705f3cd1c15286845e2aab0ceb9acdeb111
Kwamina Apenkwah <kwameapenk@gmail.com>
2024-05-17 09:52:55 -0400

    My initial first commit

---
+++ src/contact.js
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom";
+
+function Contact(){
+    return (
+        <div>
+            This is the Contact page
+            <Link to = {"/"}> Home Page</Link>
+            </div>
+    );
+}
+
+export default Contact;
---
+++ src/course.js
@@ -0,0 +1,11 @@
+
+function Course(props){
+    return (
+        <div>
+        <p>{props.name}</p>
+       
+       </div>
+    );
+}
+
+export default Course;
